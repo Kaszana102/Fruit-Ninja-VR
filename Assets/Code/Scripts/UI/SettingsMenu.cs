41a3e956bc59770030dde6be788d962863ea4293
Michał Kuprianowicz <michal.kuprianowicz@interia.pl>
2025-01-12 16:16:52 +0100

    added main menu and settings

---
+++ Assets/Code/Scripts/UI/SettingsMenu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class SettingsMenu : MonoBehaviour
+{
+	public Slider audioSlider;
+	private TextMeshProUGUI audioValue;
+
+
+	private void Awake()
+	{
+		audioValue = gameObject.GetNamedChild("AudioValue").GetComponent<TextMeshProUGUI>();
+
+		if (!PlayerPrefs.HasKey("Audio"))
+		{
+			Debug.Log("Audio not set, setting it to 0.5");
+			PlayerPrefs.SetFloat("Audio", 0.5f);
+			PlayerPrefs.Save();
+		}
+
+
+		audioValue.text = PlayerPrefs.GetFloat("Audio").ToString("0.00").Replace(',', '.');
+		audioSlider.value = PlayerPrefs.GetFloat("Audio");
+
+	}
+
+	private void Start()
+    {
+        
+    }
+
+	public void SetAudio(float volume)
+	{
+		float roundedVolume = (float)Math.Round(volume, 2);
+
+		PlayerPrefs.SetFloat("Audio", roundedVolume);
+		PlayerPrefs.Save();
+		audioValue.text = roundedVolume.ToString("0.00").Replace(',', '.');
+		// Debug.Log("Changed audio volume to " + roundedVolume);
+	}
+
+
+}
---
+++ Assets/Code/Scripts/UI/UIManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager instance;
+    private GameObject deathUI;
+    private GameObject winningUI;
+    private Text respawnText;
+    private Text summaryText;
+
+    public static UIManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<UIManager>();
+            }
+            return instance;
+        }
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        deathUI = gameObject.GetNamedChild("DeathUI");
+        winningUI = gameObject.GetNamedChild("WinningUI");
+
+        respawnText = deathUI.GetNamedChild("RespawnText").GetComponent<Text>();
+        summaryText = winningUI.GetNamedChild("SummaryText").GetComponent<Text>();
+
+        deathUI.SetActive(false);
+        winningUI.SetActive(false);
+    }
+
+    /// <summary>
+    /// shows death UI text
+    /// </summary>    
+    public void ShowDeathUI()
+    {
+        deathUI.SetActive(true);
+    }
+
+    /// <summary>
+    /// hides death UI text
+    /// </summary>    
+    public void HideDeathUI()
+    {
+        deathUI.SetActive(false);
+    }
+
+
+    /// <summary>
+    /// shows winning UI text
+    /// </summary>    
+    public void ShowWinningUI()
+    {
+        winningUI.SetActive(true);
+    }
+
+    /// <summary>
+    /// hides winning UI text
+    /// </summary>    
+    public void HideWinningUI()
+    {
+        winningUI.SetActive(false);
+    }
+
+    /// <summary>
+    /// updates text afetr player dies
+    /// </summary>    
+    public void UpdateRespawnText(float seconds)
+    {
+        respawnText.text = "Respawning in: " + seconds + " seconds";
+    }
+
+    public void SetSummaryText(float elapsedSeconds, int deaths)
+    {
+        float minutes = Mathf.FloorToInt(elapsedSeconds / 60);
+        float seconds = Mathf.FloorToInt(elapsedSeconds % 60);
+
+
+        string timeToDisplay = string.Format("{0:00}:{1:00}", minutes,seconds);
+
+        summaryText.text = "You completed the maze in "+ timeToDisplay + " and "+ deaths+" deaths.";
+    }
+}
