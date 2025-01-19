e179d921633fa477c1116c51f38d0c9c7d3e8705
Kaszana102 <96295132+Kaszana102@users.noreply.github.com>
2025-01-19 20:59:38 +0100

    post lab changes

--- Assets/Code/Scripts/Gestures/GesturePoint.cs
+++ Assets/Code/Scripts/Gestures/GesturePoint.cs
@@ -12,7 +12,7 @@ public class GesturePoint : MonoBehaviour
     public GesturePoint prevPoint, nextPoint;
 
     public static readonly float maxDistance = 0.2f;
-    public static readonly float maxAngle = 20f;
+    public static readonly float maxAngle = 30f;
 
     [HideInInspector]
     public bool activated = false;
@@ -46,11 +46,11 @@ public class GesturePoint : MonoBehaviour
 
     bool ShouldActivate()
     {
-        Quaternion handRot = hand.rotation * Quaternion.Euler(90, 0, 0);
+        Quaternion handRot = hand.rotation;// * Quaternion.Euler(90, 0, 0);
 
         if ((Vector3.Distance(targetPos, hand.position) < maxDistance)
-                    //&&
-                    //(Quaternion.Angle(targetRot, handRot) < maxAngle)
+                    &&
+                    (Quaternion.Angle(targetRot, handRot) < maxAngle)
                     )
         {
             Debug.Log("ACTIVATED");
@@ -77,9 +77,12 @@ public class GesturePoint : MonoBehaviour
     // moved from update
     void OnTriggerStay(Collider other)
     {
+        Quaternion handRot1 = other.transform.rotation * Quaternion.Euler(90,0, 0) * Quaternion.Euler(0, 90, 0);
+        Quaternion handRot2 = other.transform.rotation * Quaternion.Euler(90, 0, 0) * Quaternion.Euler(0, -90, 0);
         if (other.CompareTag("Sword") && !activated)
         {
-            if (prevPoint == null || prevPoint.activated)
+            if ((prevPoint == null || prevPoint.activated) && (Quaternion.Angle(targetRot, handRot1) < maxAngle || Quaternion.Angle(targetRot, handRot2) < maxAngle))
+            //if ((prevPoint == null || prevPoint.activated) && Quaternion.Angle(targetRot, handRot1) < maxAngle)
             {
                 OnActivate();
             }
